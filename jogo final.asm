beea3b94f531519b6c8e5f7c3a7f917baf47e457
Your Name <you@example.com>
2024-09-27 08:11:29 -0300

    Alterações

--- jogo final.asm
+++ jogo final.asm
@@ -16,16 +16,64 @@
                 db '                   |___/              ',13,10
                 db '                                      ',13,10
                 
-    ,menu_lenght EQU $-menu
+    menu_lenght EQU $-menu
                 
                 
-    menuInicial db '                                      ',13,10
-                db '                                      ',13,10
-                db '                                      ',13,10
-                db'          '218,196,196,218'            ',13,10     
-                db'          '221,'Jogar',221'            ',13,10
-                db'          '192,196,196,192'            ',13,10
-                db'          '218,196,196,218'            ',13,10     
-                db'          '221,'Sair ',221'            ',13,10
-                db'          '192,196,196,192'            ',13,10
-    menuInicial EQU $-menuInicial
+    menuInicial db '',13,10
+                db '',13,10
+                db '',13,10
+                db          218,196,196,218          ,13,10     
+                db          221,' Jogar',221         ,13,10
+                db          192,196,196,192          ,13,10
+                db          218,196,196,218          ,13,10     
+                db          221,' Sair',221          ,13,10
+                db          192,196,196,192          ,13,10
+    menuInicial_lenght EQU $-menuInicial
+
+.code
+
+print_logo_inicial PROC
+
+    push AX
+    push SI
+    push CX
+    push DI
+
+    mov bp, OFFSET menuInicial    ; ES:BP MENSAGEM
+    mov ah, 13h                     ; int 13h - escreve string
+    mov al, 01h                     ; atributo em bl, move cursor
+    xor bh, bh                      ; pagina de video 0
+    mov bl, 0AH                     ; attribute 
+    mov cx, menuInicial_lenght     ; tamanho da string
+    mov dh, 1                       ; linha string
+    mov dl, 1                       ; coluna string
+    int 10h                         ; chama bios
+    
+    pop DI
+    pop CX
+    pop SI
+    pop AX
+
+    ret
+
+print_logo_inicial ENDP
+
+video PROC
+    push AX
+    
+    mov AX,13H
+    int 10H 
+          
+    pop AX  
+    ret
+video ENDP
+
+inicio:
+    mov AX,@DATA
+    mov DS,AX
+    call 
+    call print_logo_inicial
+    mov AH,4CH
+    int 21h
+    
+end inicio
